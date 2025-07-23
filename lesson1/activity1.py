a384a5283c1cf3b120c30a2cc640e2ce6ef197a2
zuhair2808 <zuhair2808@gmail.com>
2025-07-23 21:07:35 +0600

    lesson1

---
+++ lesson1/activity1.py
@@ -0,0 +1,15 @@
+print("****************************")
+print("Student profile")
+print("****************************\n")
+school = "Scholastica"
+print("\t\tSchool:", school)
+name = "Zuhair"
+grade = 7
+section = "indigo"
+ID = "2022020202"
+
+print ("Name:", name)
+print ("Grade:", grade)
+print ("Section:", section)
+print ("ID:", ID)
+print("\n****************************")
---
+++ lesson1/activity2.py
@@ -0,0 +1,7 @@
+x = 7
+y = 4
+print ( x + y )
+firstname = "Zuhair"
+lastname =  "Rahman"
+print( firstname, lastname)
+print (f"x + y = {x + y}")
