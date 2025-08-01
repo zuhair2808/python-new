156e270ad6abb16aa967bac19aa0374e376d1753
zuhair2808 <zuhair2808@gmail.com>
2025-08-01 19:40:13 +0600

    project1

---
+++ project1/project1.py
@@ -0,0 +1,13 @@
+print("****************************")
+print("Resume")
+print("****************************\n")
+firstname = "Zuhair"
+lastname = "Rahman"
+hoursabletowork = 6
+previousjobs = "KFC"
+
+print ("First name:", firstname)
+print ("Last name:", lastname)
+print ("Hours able to work:", hoursabletowork)
+print ("Previous jobs:", previousjobs)
+print("\n****************************")
